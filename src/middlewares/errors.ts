2d4b4a0346cf9b10d95a7a60a4e1b06261590c79
Raz Buchnik <raz@taxi4you.co.il>
2021-10-05 18:26:50 +0300

    fix

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -1,20 +1,27 @@
 import { Request, Response, NextFunction } from 'express'
+import { Logger } from 'winston'
 import { errorHandler } from '../services'
 
 export const errorsRouter = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
+  logger: Logger
 ) => {
-  // if no err, next
-  if (!err) {
-    return next()
+  return (
+    err: any,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+
+    // if no err, next
+    if (!err) {
+      return next()
+    }
+
+    // handle error
+    const { castError } = errorHandler({ err, logger })
+
+    // // send the error
+    res.status(castError.error.code).json(castError)
+    
   }
-
-  // handle error
-  const { castError } = errorHandler({ err })
-
-  // send the error
-  res.status(castError.error.code).json(castError)
 }
--- src/services/error-handler.ts
+++ src/services/error-handler.ts
@@ -1,8 +1,10 @@
 import { CastError } from '../cast'
+import { Logger } from 'winston'
 import { CustomError, ServerError } from '../errors'
 
 export interface ErrorHandlerParams {
-  err: any | CustomError
+  err: any | CustomError,
+  logger: Logger
 }
 
 export interface ErrorHandlerResult {
@@ -11,23 +13,38 @@ export interface ErrorHandlerResult {
 
 export const errorHandler = ({
   err,
+  logger
 }: ErrorHandlerParams): ErrorHandlerResult => {
   // if no error
   if (!err) return {}
 
-  // if programmer error
-  if (!err?.is_operational) {
-    if (err?.stack) {
-      console.error(err.stack)
+  // if err is not operational - a programmer error
+  // 1. log it.
+  // 2. overwrite it with a server error.
+  if (err?.is_operational !== true) {
+    if (err instanceof SyntaxError) {
+      logger.error(`SyntaxError was just catched by express errors middleware:`, err)
+    } else if (err instanceof RangeError) {
+      logger.error(`RangeError was just catched by express errors middleware:`, err)
+    } else if (err instanceof ReferenceError) {
+      logger.error(`ReferenceError was just catched by express errors middleware:`, err)
+    } else if (err instanceof TypeError) {
+      logger.error(`TypeError was just catched by express errors middleware:`, err)
+    } else if (err instanceof URIError) {
+      logger.error(`URIError was just catched by express errors middleware:`, err)
+    } else if (err instanceof Error) {
+      logger.error(`Error was just catched by express errors middleware:`, err)
     }
     err = new ServerError()
   }
 
-  // cast the error
-  const castError = new CastError(err)
+  // escape is_operational from the client - if any
+  try {
+    delete err.is_operational
+  } catch (err) {}
 
-  // escape the is_operational error from the client
-  delete castError.error.is_operational
+  // get cast instance
+  const castError = new CastError(err)
 
   // send it back
   return { castError }
